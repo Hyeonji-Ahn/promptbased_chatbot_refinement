9e28c35082b086c74484a397cc89839e19d0ae9c
Hyeonji-Ahn <66628832+Hyeonji-Ahn@users.noreply.github.com>
2025-02-11 23:06:08 -0500

    basic UI

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { redirect, useRouter } from "next/navigation";
+export default function ResourceCalendar() {
+    const router = useRouter();
+    router.push("/chatInterface");
+}
